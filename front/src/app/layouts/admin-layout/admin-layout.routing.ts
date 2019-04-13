23acfe6d5363902cb8d5defbe9c098b2cecd9e64
Krystian Szabat <221662@edu.p.lodz.pl>
2019-04-13 10:40:31 +0200

    [FRONT][SZABAT] - Example views and actions 3/4 (edit client)

--- front/src/app/layouts/admin-layout/admin-layout.routing.ts
+++ front/src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -6,6 +6,7 @@ import { AccountComponent } from '../../modules/ror/account/list/account.compone
 import { AccountsingleComponent } from '../../modules/ror/account/accountsingle/accountsingle.component';
 import { ClientsListComponent } from '../../modules/ror/clients/clients-list/clients-list.component';
 import { ClientsAddComponent } from '../../modules/ror/clients/clients-add/clients-add.component';
+import { ClientsEditComponent } from '../../modules/ror/clients/clients-edit/clients-edit.component';
 
 export const AdminLayoutRoutes: Routes = [
     { path: 'dashboard', component: DashboardComponent },
@@ -13,4 +14,5 @@ export const AdminLayoutRoutes: Routes = [
     { path: 'accounts/:id', component: AccountsingleComponent },
     { path: 'clients', component: ClientsListComponent },
     { path: 'client', component: ClientsAddComponent },
+    { path: 'client/:id', component: ClientsEditComponent },
 ];
