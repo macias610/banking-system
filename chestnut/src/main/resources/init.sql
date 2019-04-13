da33b74d62e039c928c3992d1fd4e97dda5e19b0
Piotr Kaźmierczak <piotrkazmierczak@piotrs-macbook-air.home>
2019-04-13 16:22:13 +0200

    init.sql change (table names' letter size)

--- chestnut/src/main/resources/init.sql
+++ chestnut/src/main/resources/init.sql
@@ -1,2 +1,2 @@
-INSERT IGNORE INTO Banks (id, name, swift) VALUES (1, 'Chestnut bank', '20879576');
-INSERT IGNORE INTO Users (id, employee_number, first_name, surname, bank_id) VALUES (1, UUID(), 'Nikola', 'Kowalska', 1);
+INSERT IGNORE INTO banks (id, name, swift) VALUES (1, 'Chestnut bank', '20879576');
+INSERT IGNORE INTO users (id, employee_number, first_name, surname, bank_id) VALUES (1, UUID(), 'Nikola', 'Kowalska', 1);
