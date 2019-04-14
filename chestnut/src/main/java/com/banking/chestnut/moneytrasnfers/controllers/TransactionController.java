13f3d3aabec375d3ad911678d3b970c1c7d6ce7a
piotrkazmierczak <pkazmierczak13@gmail.com>
2019-04-14 11:34:00 +0200

    added TransactionController

---
+++ chestnut/src/main/java/com/banking/chestnut/moneytrasnfers/controllers/TransactionController.java
@@ -0,0 +1,9 @@
+package com.banking.chestnut.moneytrasnfers.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="/transactions")
+public class TransactionController {
+}
