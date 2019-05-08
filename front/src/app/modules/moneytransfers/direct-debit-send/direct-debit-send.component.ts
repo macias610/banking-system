ba52a629f9f070f0a27caab4072422f1f72d6a6a
Krystian Szabat <221662@edu.p.lodz.pl>
2019-05-08 23:49:53 +0200

    [FIX] - Direct debit send

--- front/src/app/modules/moneytransfers/direct-debit-send/direct-debit-send.component.ts
+++ front/src/app/modules/moneytransfers/direct-debit-send/direct-debit-send.component.ts
@@ -1,72 +1,88 @@
-import {Component, OnInit} from '@angular/core';
-import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
-import {TransfersService} from '../transfers.service';
-import {DirectDebitListItem} from '../../../models/transfer/directDebitListItem';
-import {ResponseData} from '../../../models/responseData';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {HttpClient} from '@angular/common/http';
-import {Provider} from '../../../models/account/provider';
-import {NotificationService} from '../../../shared/services/notification.service';
+import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { TransfersService } from '../transfers.service';
+import { DirectDebitListItem } from '../../../models/transfer/directDebitListItem';
+import { ResponseData } from '../../../models/responseData';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Provider } from '../../../models/account/provider';
+import { NotificationService } from '../../../shared/services/notification.service';
 
 @Component({
-  selector: 'app-direct-debit-send',
-  templateUrl: './direct-debit-send.component.html',
-  styleUrls: ['./direct-debit-send.component.scss']
+    selector: 'app-direct-debit-send',
+    templateUrl: './direct-debit-send.component.html',
+    styleUrls: ['./direct-debit-send.component.scss']
 })
 export class DirectDebitSendComponent implements OnInit {
 
-  formInSave = false;
-  sendTransferForm: FormGroup;
-  providers: Observable<Account[]>;
-  directDebits: Observable<DirectDebitListItem[]>;
-  selectedValue: Provider;
+    formInSave = false;
+    sendTransferForm: FormGroup;
+    providers: Provider[] = [];
+    directDebits: Observable<DirectDebitListItem[]>;
+    selectedValue: Provider;
 
-  constructor(private service: TransfersService
-              , private fb: FormBuilder
-              , private http: HttpClient
-              , private notiService: NotificationService) {
-  }
+    constructor(private service: TransfersService
+        , private fb: FormBuilder
+        , private http: HttpClient
+        , private notiService: NotificationService) {
+    }
 
-  ngOnInit() {
-    this.sendTransferForm = this.fb.group({
-      'senderAccNumber': ['', [Validators.required]],
-      'value': ['', [Validators.required]],
-      'title': ['', [Validators.required]],
-      'receiverAccNumber': ['', []],
-      'transactionDate': ['', [Validators.required]],
-    });
-    this.providers = this.service.getProviders().pipe(
-      map(item => item.data)
-    );
-  }
+    ngOnInit() {
+        this.sendTransferForm = this.fb.group({
+            'senderAccNumber': ['', [Validators.required]],
+            'value': ['', [Validators.required]],
+            'title': ['', [Validators.required]],
+            'receiverAccNumber': ['', []],
+            'transactionDate': ['', [Validators.required]],
+        });
 
-  onChange() {
-    this.directDebits = this.service.getDirectDebitsForProvider(this.selectedValue.id).pipe(
-      map(item => item.data)
-    );
-  }
+        this.service.getProviders().subscribe(
+            (data: ResponseData) => {
+                this.providers = data.data;
+            },
+            (error) => {
 
+            }
+        );
+    }
 
-  sendTransfer() {
-    this.sendTransferForm.patchValue({
-      'receiverAccNumber': this.selectedValue.accountNumber
-    });
-    const formValue = this.sendTransferForm.value;
-    this.formInSave = true;
+    onChange(providerId) {
+        if (providerId) {
+            this.selectedValue = this.providers.find(item => parseInt(item.id, 10) === parseInt(providerId, 10));
+            this.directDebits = this.service.getDirectDebitsForProvider(providerId).pipe(
+                map(item => item.data),
+                catchError(err => { this.showError(err); return of([]); })
+            );
+        } else {
+            this.selectedValue = null;
+        }
+    }
 
-    this.service.sendTransfer(formValue).subscribe(
-      (data: ResponseData) => {
-        this.formInSave = false;
-        this.sendTransferForm.reset();
-        this.notiService.showNotification(data.notification || '', true);
-      },
-      (error) => {
+    showError(error) {
         const errorData: ResponseData = error.error;
-        console.log(errorData);
         this.formInSave = false;
         this.notiService.showNotification(errorData ? errorData.notification : '', false);
-      }
-    );
-  }
+    }
+
+    sendTransfer() {
+        this.sendTransferForm.patchValue({
+            'receiverAccNumber': this.selectedValue.accountNumber
+        });
+        const formValue = this.sendTransferForm.value;
+        this.formInSave = true;
+
+        this.service.sendTransfer(formValue).subscribe(
+            (data: ResponseData) => {
+                this.formInSave = false;
+                this.sendTransferForm.reset();
+                this.notiService.showNotification(data.notification || '', true);
+            },
+            (error) => {
+                const errorData: ResponseData = error.error;
+                this.formInSave = false;
+                this.notiService.showNotification(errorData ? errorData.notification : '', false);
+            }
+        );
+    }
 }
